8f578a6e107af747aa2967dc475264fb97c5a0fe
CodeBlueOcean <95150255+CodeBlueOcean@users.noreply.github.com>
2022-03-18 21:48:31 -0500

    Add files via upload

---
+++ super().py
@@ -0,0 +1,31 @@
+class User(object):
+    def __init__(self, email):
+        self.email = email 
+
+    def sign_in(self):
+        print('logged in')
+
+    def attack(self):
+        print('do nothing')
+
+class Wizard(User):
+    def __init__(self, name, power, email):
+        super().__init__(email)
+        # User.__init__(self, email)
+        self.name = name
+        self.power = power
+
+    def attack(self):
+        User.attack(self)
+        print(f'attacking with power of {self.power}')
+      
+
+class Archer(User):
+    def __init__(self, name, num_arrows):
+        self.name = name
+        self.num_arrows = num_arrows
+    def attack(self):
+        print(f'attacking with arrows: arrows left- {self.num_arrows}')
+
+wizard1 = Wizard('Merlin', 60, 'merlin@gmail.com')
+print(wizard1.email)
